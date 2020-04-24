2bab5c564f5319a2985a07132cb467de0141a6a0
fruitsnack1 <tomas.ryzner@email.cz>
2020-04-24 23:55:09 +0200

    WIP

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
+const fileUpload = require('express-fileupload')
+const path = require('path')
 
+app.use(fileUpload());
 app.use(express.json())
-app.use(express.static(__dirname + '/'));
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.set('view engine', 'jade');
 
 const indexRouter = require('./routes/index')
 app.use('/', indexRouter)
--- routes/index.js
+++ routes/index.js
@@ -19,54 +19,70 @@ connection.connect((err) => {
 })
 
 router.get('/', (req, res) => {
+    res.render('index')
+})
+
+router.get('/registrace', (req, res) => {
+    res.render('registrace')
+})
+
+router.get('/login', (req, res) => {
+    res.render('login2')
+})
+
+router.get('/', (req, res) => {
     connection.query('SELECT * FROM test', (error, results, fields) => {
-        if (error) throw error
+        if (error) return console.log(error)
         res.json(results)
     })
 })
 
 router.get('/sjezdovky', (req, res) => {
-    connection.query('SELECT * FROM Sjezdovka', (error, results) => {
-        if (error) throw error
-        res.json(results)
+    connection.query('SELECT * FROM Sjezdovky s LEFT JOIN Stavysjezdovek ss ON s.SjezdovkaID = ss.SjezdovkaID', (error, results) => {
+        if (error) return console.log(error)
+        results.map(sjezdovka => {
+            sjezdovka.Otevrenado = sjezdovka.Otevrenado.substring(0, 5)
+            sjezdovka.Otevrenaod = sjezdovka.Otevrenaod.substring(0, 5)
+        })
+        console.log(JSON.parse(JSON.stringify(results)))
+        results = JSON.parse(JSON.stringify(results))
+        res.render('sjezdovky', { sjezdovky: results })
     })
 })
 
 router.get('/vleky', (req, res) => {
     connection.query('SELECT * FROM Vleky', (error, results) => {
-        if (error) throw error
+        if (error) return console.log(error)
         res.json(results)
     })
 })
 
 router.get('/sjezdovka/stav/:id', (req, res) => {
-    connection.query(`SELECT * FROM Stavsjezdovky WHERE StavsjezdovkyID = ${req.params.id}`, (error, results) => {
-        if (error) throw error
+    connection.query(`SELECT * FROM Stavysjezdovek WHERE StavsjezdovkyID = ${req.params.id}`, (error, results) => {
+        if (error) return console.log(error)
         res.json(results)
     })
 })
 
 router.post('/zakaznik', (req, res) => {
-    console.log(req.body)
+    console.log(req.body.birthdate)
     req.body.password = cryptoJs.SHA256(req.body.password).toString()
-    let query = `CALL addZakaznik("${req.body.firstname}","${req.body.lastname}","${req.body.email}","${req.body.phone}","${req.body.birthdate}","${req.body.username}","${req.body.password}","${req.body.street}","${req.body.cp}","${req.body.city}","${req.body.psc}",)`
-    console.log(query)
     connection.query(`CALL addZakaznik("${req.body.firstname}","${req.body.lastname}","${req.body.email}","${req.body.phone}","${req.body.birthdate}","${req.body.username}","${req.body.password}","${req.body.street}",${req.body.cp},"${req.body.city}",${req.body.psc})`, (error, results) => {
-        if (error) throw error
-        res.json(req.body)
+        if (error) return console.log(error)
+        res.render('login')
     })
 })
 
 router.post('/login', (req, res) => {
     connection.query(`SELECT prihlasovaciHeslo, ZakaznikID FROM Zakaznici WHERE prihlasovaciJmeno = "${req.body.username}"`, (error, results) => {
-        if (error) throw error
+        if (error) return console.log(error)
         const hash = cryptoJs.SHA256(req.body.password).toString()
         if (hash === results[0].prihlasovaciHeslo) {
             const tokenUser = { 'id': results[0].ZakaznikID }
             const accessToken = auth.generateAccessToken(tokenUser)
 
             res.cookie('accessToken', accessToken);
-            res.status(200).json({ message: 'logged in' })
+            res.render('home')
         } else
             res.status(500).json({ message: 'wrong password' })
     })
@@ -74,6 +90,16 @@ router.post('/login', (req, res) => {
 
 router.get('/protected', auth.authenticateToken, (req, res) => {
     res.send('protected')
+})
+
+router.get('/image', (req, res) => {
+    res.render('image')
+})
+
+router.post('/image', (req, res) => {
+    console.log(req.files)
+    res.send('cc')
+
 })
 
 
